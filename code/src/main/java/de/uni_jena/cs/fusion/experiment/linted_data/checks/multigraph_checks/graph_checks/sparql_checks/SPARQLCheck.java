f4af9e2b30769648b45bb319ae674ae212be7b7e
Merle Gänßinger <merle.gaenssinger@uni-jena.de>
2022-10-20 22:34:48 +0200

    Added SPARQLAskCheck and SPARQLSelectCheck
    Represent the two different types of SPARQL queries

--- code/src/main/java/de/uni_jena/cs/fusion/experiment/linted_data/checks/multigraph_checks/graph_checks/sparql_checks/SPARQLCheck.java
+++ code/src/main/java/de/uni_jena/cs/fusion/experiment/linted_data/checks/multigraph_checks/graph_checks/sparql_checks/SPARQLCheck.java
@@ -2,15 +2,7 @@ package de.uni_jena.cs.fusion.experiment.linted_data.checks.multigraph_checks.gr
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
-import org.apache.jena.query.QueryExecution;
-import org.apache.jena.query.QueryExecutionFactory;
-import org.apache.jena.query.QueryType;
-import org.apache.jena.query.ResultSet;
-import org.apache.jena.rdf.model.Model;
-
-import de.uni_jena.cs.fusion.experiment.linted_data.JUnitXML.Failure;
 import de.uni_jena.cs.fusion.experiment.linted_data.checks.multigraph_checks.graph_checks.GraphCheck;
 import de.uni_jena.cs.fusion.experiment.linted_data.types.Level;
 import de.uni_jena.cs.fusion.experiment.linted_data.types.Severity;
@@ -24,19 +16,16 @@ public abstract class SPARQLCheck extends GraphCheck {
 	/**
 	 * the query that will be executed against the model
 	 */
-	private final String query;
-	private final QueryType type;
+	protected final String query;
 
-	protected SPARQLCheck(Level level, TargetLanguage targetLanguage, Severity severity, String name, String query, QueryType type) {
+	protected SPARQLCheck(Level level, TargetLanguage targetLanguage, Severity severity, String name, String query) {
 		super(level, targetLanguage, severity, name);
 		this.query = query;
-		this.type = type;
 	}
 
-	protected SPARQLCheck(Level level, TargetLanguage targetLanguage, Severity severity, String name, File queryFile, QueryType type) {
+	protected SPARQLCheck(Level level, TargetLanguage targetLanguage, Severity severity, String name, File queryFile) {
 		super(level, targetLanguage, severity, name);
 		this.query  = readQuery(queryFile);
-		this.type = type;
 	}
 
 	/**
@@ -54,22 +43,5 @@ public abstract class SPARQLCheck extends GraphCheck {
 			return null;
 		}
 	}
-
-	@Override
-	public List<Failure> execute(Model model, String failureDescription) {
-		QueryExecution queryExecution = QueryExecutionFactory.create(query, model);
-		switch(type) {
-		case ASK:
-			return execute(queryExecution.execAsk(), failureDescription); 
-		case SELECT:
-			return execute(queryExecution.execSelect(), failureDescription);
-		default:
-			return execute(queryExecution.execSelect(), failureDescription);
-		}
-	}
-
-	protected abstract List<Failure> execute(ResultSet rs, String failureDescription);
-	
-	protected abstract List<Failure> execute(boolean b, String failureDescription);
 
 }
---
+++ code/src/main/java/de/uni_jena/cs/fusion/experiment/linted_data/checks/multigraph_checks/graph_checks/sparql_checks/SPARQLSelectCheck.java
@@ -0,0 +1,35 @@
+package de.uni_jena.cs.fusion.experiment.linted_data.checks.multigraph_checks.graph_checks.sparql_checks;
+
+import java.io.File;
+import java.util.List;
+
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.query.ResultSet;
+
+import de.uni_jena.cs.fusion.experiment.linted_data.JUnitXML.Failure;
+import de.uni_jena.cs.fusion.experiment.linted_data.types.Level;
+import de.uni_jena.cs.fusion.experiment.linted_data.types.Severity;
+import de.uni_jena.cs.fusion.experiment.linted_data.types.TargetLanguage;
+
+public abstract class SPARQLSelectCheck extends SPARQLCheck {
+
+	public SPARQLSelectCheck(Level level, TargetLanguage targetLanguage, Severity severity, String name, String query) {
+		super(level, targetLanguage, severity, name, query);
+	}
+
+	public SPARQLSelectCheck(Level level, TargetLanguage targetLanguage, Severity severity, String name,
+			File queryFile) {
+		super(level, targetLanguage, severity, name, queryFile);
+	}
+
+	@Override
+	public List<Failure> execute(Model model, String failureDescription) {
+		QueryExecution queryExecution = QueryExecutionFactory.create(query, model);
+		return execute(queryExecution.execSelect(), failureDescription);
+	}
+
+	protected abstract List<Failure> execute(ResultSet resultSet, String failureDescription);
+
+}
