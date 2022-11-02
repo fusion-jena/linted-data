0661782a2ed6a4757b490bb7ee8fc52be44642b2
Merle Gänßinger <merle.gaenssinger@uni-jena.de>
2022-11-02 18:04:51 +0100

    updated CheckLeadingOrTrailingSpaces
    moved query in a resource file

--- code/src/main/java/de/uni_jena/cs/fusion/experiment/linted_data/checks/multigraph_checks/graph_checks/sparql_checks/CheckLeadingOrTrailingSpaces.java
+++ code/src/main/java/de/uni_jena/cs/fusion/experiment/linted_data/checks/multigraph_checks/graph_checks/sparql_checks/CheckLeadingOrTrailingSpaces.java
@@ -1,5 +1,6 @@
 package de.uni_jena.cs.fusion.experiment.linted_data.checks.multigraph_checks.graph_checks.sparql_checks;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,36 +17,30 @@ import de.uni_jena.cs.fusion.experiment.linted_data.types.TargetLanguage;
  * 
  * 
  * \s doesn't exist in SPARQL so the equivalent form [\r\n\t\f\v ] is used
+ * <p>
  * \v also doesn't exist in SPARQL and is hence not checked
  */
 public class CheckLeadingOrTrailingSpaces extends SPARQLSelectCheck {
-	
+
 	private static final String literal = "literal";
-	private static final String subject = "s";
-	private static final String predicate = "p";
-	private static final String object = "o";
-	
+	private static final String subject = "subject";
+	private static final String predicate = "predicate";
+	private static final String object = "object";
+
 	public CheckLeadingOrTrailingSpaces() {
-		super(Level.SPARQL, TargetLanguage.RDFS, Severity.INFO, "Literals shouldn't start or end with white spaces", 
-				"SELECT DISTINCT ?" + literal + " ?" + subject + " ?" +predicate +" ?" + object + " WHERE {\n" + 
-				"  {\n" + 
-				"	SELECT (STR(?" + object +") AS ?" + literal + ") ?" + subject + " ?" +predicate +" ?" + object+ " WHERE {\n" + 
-				"		?" + subject + " ?" +predicate +" ?" + object + ".\n" + 
-				"		FILTER(isLiteral(?" + object + "))\n" + 
-				"	}\n" + 
-				"	}\n" + 
-				"  	FILTER (regex(?" + literal + ", \"^[\\r\\n\\t\\f ]|[\\r\\n\\t\\f ]$\"))\n" + 
-				"}"
-				); 
-	}	
-	
+		super(Level.SPARQL, TargetLanguage.RDFS, Severity.INFO, "Literals shouldn't start or end with white spaces",
+				new File(CheckLeadingOrTrailingSpaces.class.getClassLoader()
+						.getResource("CheckLeadingOrTrailingSpaces.rq").getFile()));
+	}
+
 	@Override
 	protected List<Failure> execute(ResultSet rs, String failureDescription) {
 		List<Failure> failures = new ArrayList<Failure>();
-		while(rs.hasNext()) {
+		while (rs.hasNext()) {
 			String description = failureDescription + "\n";
 			QuerySolution qs = rs.next();
-			description += qs.get(subject).toString() + " " + qs.get(predicate).toString() + " " + qs.get(object).toString() + "\n";
+			description += qs.get(subject).toString() + " " + qs.get(predicate).toString() + " "
+					+ qs.get(object).toString() + "\n";
 			Failure f = new Failure(this.name, this.severity, qs.getLiteral(literal).getLexicalForm(), description);
 			failures.add(f);
 		}
