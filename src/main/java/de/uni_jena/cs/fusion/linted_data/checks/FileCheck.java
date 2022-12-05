0008a9d0aeac84dfb3bc59ad91c593e58e270816
Merle Gänßinger <merle.gaenssinger@uni-jena.de>
2022-12-05 10:53:35 +0100

    updated documentation
    extended documentation on FileCheck
    added documentation to Scope

--- src/main/java/de/uni_jena/cs/fusion/linted_data/checks/FileCheck.java
+++ src/main/java/de/uni_jena/cs/fusion/linted_data/checks/FileCheck.java
@@ -16,21 +16,31 @@ import de.uni_jena.cs.fusion.linted_data.types.Scope;
  * For errors that can't be detected, once the file is parsed
  */
 public abstract class FileCheck extends Check {
-	
+
+	/**
+	 * what kind of check is created
+	 * 
+	 * all other validators extend this class
+	 */
 	private Level level;
+
+	/**
+	 * to which modeling language the check is applicable
+	 */
 	private Scope scope;
-	
+
 	/**
 	 * severity of the check
 	 */
 	protected Severity severity;
-	
+
 	/**
+	 * generate a new FileCheck
 	 * 
-	 * @param level
-	 * @param scope
-	 * @param severity
-	 * @param name general description of the testcase
+	 * @param level    what kind of check is created
+	 * @param scope    addressed modelling language
+	 * @param severity how important it is to fix occuring failures
+	 * @param name     general description of the testcase
 	 */
 	protected FileCheck(Level level, Scope scope, Severity severity, String name) {
 		super(name);
@@ -38,8 +48,18 @@ public abstract class FileCheck extends Check {
 		this.scope = scope;
 		this.severity = severity;
 	}
-	
-	public final List<Failure> startExecution(File file) throws Exception{
+
+	/**
+	 * executes the validation, collects all found errors into a list and measures
+	 * the run time
+	 * 
+	 * @param file to be processed
+	 * @return list of failures matching the criteria of the check
+	 * @throws Exception in case of an error during parsing
+	 *                   <p>
+	 *                   can't occur during runtime
+	 */
+	public final List<Failure> startExecution(File file) throws Exception {
 		long start = System.currentTimeMillis();
 		String failureDescription;
 		try {
@@ -51,22 +71,24 @@ public abstract class FileCheck extends Check {
 		this.time = System.currentTimeMillis() - start;
 		return failures;
 	}
-	
+
 	/**
+	 * executes the validation and collects all found errors into a list
 	 * 
-	 * @param file
+	 * @param file               to be processed
 	 * @param failureDescription describes the failure
+	 *                           <p>
+	 *                           the parameter is updated through the calls of
+	 *                           parent methods
 	 * 
-	 * the parameter is updated through the calls of parent methods
-	 * 
-	 * @return 
+	 * @return list of failures matching the criteria of the check
 	 */
 	public abstract List<Failure> execute(File file, String failureDescription) throws Exception;
-	
+
 	public Level getLevel() {
 		return level;
 	}
-	
+
 	public Scope getScope() {
 		return scope;
 	}
--- src/main/java/de/uni_jena/cs/fusion/linted_data/types/Scope.java
+++ src/main/java/de/uni_jena/cs/fusion/linted_data/types/Scope.java
@@ -1,7 +1,10 @@
 package de.uni_jena.cs.fusion.linted_data.types;
 
+/**
+ * modeling languages of the semantic web
+ */
 public enum Scope {
 	RDF, 
-	RDFS,
+	RDFS, 
 	OWL;
 }
