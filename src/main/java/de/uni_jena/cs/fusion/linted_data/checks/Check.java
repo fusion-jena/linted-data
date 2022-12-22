012501eba1541e3ca2397b10e83d9b7efb74c235
Merle Gänßinger <merle.gaenssinger@uni-jena.de>
2022-12-22 17:24:22 +0100

    added license

--- src/main/java/de/uni_jena/cs/fusion/linted_data/checks/Check.java
+++ src/main/java/de/uni_jena/cs/fusion/linted_data/checks/Check.java
@@ -1,10 +1,26 @@
+/**
+ * Copyright 2022 Merle Gänßinger (merle.gaenssinger@uni-jena.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.uni_jena.cs.fusion.linted_data.checks;
 
 /**
- * generic representation of a check 
+ * generic representation of a check
  * 
- * independent from different check types, 
- * bundles all general information on a check like its ID, name and execution time
+ * independent from different check types, bundles all general information on a
+ * check like its ID, name and execution time
  */
 public abstract class Check {
 
@@ -12,20 +28,20 @@ public abstract class Check {
 	 * measurement of the execution time
 	 */
 	protected long time;
-	
+
 	protected String name;
-	
+
 	public Check(String name) {
 		this.name = name;
 		this.time = 0L;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public long getTime() {
 		return time;
 	}
-	
+
 }
--- src/main/java/de/uni_jena/cs/fusion/linted_data/types/Severity.java
+++ src/main/java/de/uni_jena/cs/fusion/linted_data/types/Severity.java
@@ -1,19 +1,41 @@
+/**
+ * Copyright 2022 Merle Gänßinger (merle.gaenssinger@uni-jena.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.uni_jena.cs.fusion.linted_data.types;
 
 /**
- * represents the severity of a failure 
+ * represents the severity of a failure
  */
 public enum Severity {
 	/**
 	 * critical failure that needs to be fixed
+	 * <p>
+	 * failure cause prevents any use of the RDF data
 	 */
-	ERROR, 
+	ERROR,
 	/**
 	 * failure that should be fixed but that is not critical
+	 * <p>
+	 * failure cause prevents use of the RDF data in many cases
 	 */
 	WARN,
 	/**
 	 * failure that can be fixed, but is not urgent
+	 * <p>
+	 * failure cause prevents use of the RDF data in some rarer cases
 	 */
 	INFO
 }
